c60d36df6714146c46f2c1f36ee52eb32100dc21
henriquedbn <henriquenoronha@gmail.com>
2024-04-12 09:53:16 -0300

    First push

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * This is a simple calculator to test functionalities and learn the language
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+    }
+}
